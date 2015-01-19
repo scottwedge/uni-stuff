c299b80b53ccf6e1779373dc451f33fd6a3e8f23
Alisa Dammer <alisa.dammer@gmail.com>
2015-01-19 19:29:12 +0100

    10 manufacturers on semiconductors market and AMD data added

--- Uni/textGet.py
+++ Uni/textGet.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import re
 
 url = "http://en.wikipedia.org/wiki/Semiconductor_sales_leaders_by_year#Ranking_for_year_2013"
 soup = BeautifulSoup(urllib.request.urlopen(url))
@@ -12,13 +13,44 @@ for year in years:
 	for row in table.find_all("tr")[1:20]:
 		cells = row.find_all("td")
 		output[year].append(cells[2].text)
-final_list = []
+
+
+#clean the dctionary from unneded info
+def clean_list(dic):
+	new = {}
+	for key in output.keys():
+		new[key] = []
+		for item in output[key]:
+			new[key].append(re.sub(r'\s?\(.*\)', '', str(item)))
+	return new
+
+
+output = clean_list(output)
+
+#get all the companies, that worked through 2000-2013
+all_companies = []
+
+
 for key in output.keys():
-	for item in output[key]:
-		if key != 2013:
-			if item in output[key+1]:
-				final_list.append(item)
+	for item in output[key]: 
+		if item not in  all_companies:
+			all_companies.append(item)
+counter = {value: 0 for value in all_companies} #initial dict
+
+#take those companies, who were listed at least 11 years out of 14
+for key in counter.keys():
+	for key_j in output.keys():
+		if key in output[key_j]:
+			counter[key] = counter[key] + 1
 		else:
-			if item not in final_list:
-				final_list.append(item)
-print(final_list)
+			counter[key] = counter[key] + 0
+final_list = []
+for key in counter.keys():
+	if counter[key] > 10:
+		final_list.append(key)
+	else:
+		pass
+
+print(final_list, len(final_list))
+
+
