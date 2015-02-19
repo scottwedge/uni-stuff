1635aa864ea8673592bf30671dd1f1169c33fcc6
Alisa Dammer <alisa.dammer@gmail.com>
2015-02-19 12:23:00 +0100

    intro for first algorithm added. Data ready

--- Uni/correlationMatrix.py
+++ Uni/correlationMatrix.py
@@ -19,16 +19,34 @@ learningSet.close()
 companies = []
 correlation_list = []
 correlation_dict = {}
-indexes = list(range(0,71))
 correlationMatrix = open('data/CorrelationMatrix.csv')
 read_matrix = csv.reader(correlationMatrix, delimiter=',')
 for row in read_matrix:
 	companies.append(row[0])
 	correlation_list.append(row[1])
-#companies_dict = {companies[]:[]}
-correlation_dict = {str(correlation_list[0]):[float(correlation_list[i]) for i in range(1,len(correlation_list))]}
-#choosing the final companies
-important_companies = []
+correlation_list.remove('Intel')
+for i in range(0,len(correlation_list)):
+	correlation_list[i] = float(correlation_list[i])
+companies.remove('')
+#get the final dictionary to work with
+correlation_dict = dict(zip(correlation_list, companies))
+#clean our correlation_list - first-order cut-off
+for coefficient in correlation_list:
+	if (coefficient < -(0.25)) or (coefficient > 0.25):
+		pass
+	else:
+		correlation_list.remove(coefficient)
+#now we only get the names of the company that stayed for further analysis
+difference = []
+for key in correlation_dict.keys():
+	if key not in correlation_list:
+		companies.remove(correlation_dict[key])
+		difference.append(correlation_dict[key])
+	else:
+		pass
+
+print(len(correlation_list), correlation_list)
+print(len(companies), companies)
 #print(correlation_dict)
-print(correlation_dict)
+print(difference)
 correlationMatrix.close()
