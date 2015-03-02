4d74fadbd7d81987e81f2fe517d62158d07eb59e
Alisa Dammer <alisa.dammer@gmail.com>
2015-03-02 20:49:45 +0100

    Stationary checked with Dikey-Fuller test, the code rewritten in functions-form

--- Uni/DataPreprocessing.py
+++ Uni/DataPreprocessing.py
@@ -3,10 +3,13 @@ import datetime
 import matplotlib.pyplot as plt 
 import numpy as num
 from scipy import stats as stat
+import statsmodels.tsa.stattools as statmodel
+from multiprocessing import Pool
 
 def getData(file_name):
 	data = pd.read_csv(str(file_name))
 	return data
+
 def formatDataIntoDict(data):
 	dict_data = {}
 	for key in data.keys():
@@ -15,12 +18,31 @@ def formatDataIntoDict(data):
 		for i in range(0,len(data[key])):
 			dict_data[key].append(float(data[key][i]))
 	return dict_data
+
 def getCompanies(data):
 	list_companies = []
 	for key in data.keys():
 		list_companies.append(key)
 	return list_companies
 
+def passes_dftest(data):
+	print("Processing {}".format(data[0]))
+	if statmodel.adfuller(data[1], 250, 'ctt', 't-stat', False, False)[0] < 1:
+		return (data[0], True)
+	else:
+		return (data[0], False)
+
+#check week stationarity and stationary with Deakey-Fuller Test
+def stationarity(dict_data):
+	stationary = {}
+	#initiate output dictionary
+	for key in dict_data.keys():
+		stationary[key] = False
+	print(stationary.keys())
+	with Pool() as p:
+		print("Starting parallel map")
+		p.map(passes_dftest, dict_data.items())
+	return stationary
 
 #Kolmogorov test 
 def kolmogorovTest(dict_data, distribution):
@@ -46,18 +68,18 @@ def checkDictionary(dictionary,listCheck):
 			result = True
 	return result
 
+if __name__ == "__main__":
+	'''Resulting outputs'''
+	f = 'data/LearningSet.csv'
+	data = getData(f)
+	dict_data = formatDataIntoDict(data)
+	list_companies = getCompanies(data)
+	first = stat.moment(dict_data['Intel'], 1,0)
+	second = stat.moment(dict_data['Intel'], 2, 0)
+	#print(len(dict_data))
+	stationary = stationarity(dict_data)
+	print(stationary)
+	#kolmogorov = kolmogorovTest(dict_data['Intel'], 'norm')
+	#print("kolmogorov: "+str(kolmogorov), "first momentt: "+str(first), "second moment: "+str(second))
 
-'''Resulting outputs'''
-f = 'data/LearningSet.csv'
-data = getData(f)
-dict_data = formatDataIntoDict(data)
-list_companies = getCompanies(data)
-kolmogorov = kolmogorovTest(dict_data, 'norm')
-print(kolmogorov)
-
-#test kolmogorov 
-test_data = dict_data['Intel']
-for item in test_data:
-	item = (item - num.mean(test_data))/num.std(test_data)
-print(stat.chisquare(dict_data['Intel']))
-print(stat.kstest(dict_data['Intel'], 'norm'))
+	#test kolmogorov 
