c9187b4c44c9a4085bf48ab4f70436571871d20c
Alisa Dammer <alisa.dammer@gmail.com>
2015-09-23 10:40:03 +0200

    first data in main

--- Uni/python/__main___.py
+++ Uni/python/__main___.py
@@ -1,13 +1,16 @@
 """__main___"""
 
-
+from DataFormating import *
 
 if __name__ == '__main__':
 
 	# initialize the data
+	raw_data = DataFormating("../data/LearningSet.csv")
+	dict_data = raw_data.keep_dict()
+	list_companies = raw_data.getAllCompanies()
+	dependent_variable, rest_companies, dict_final = raw_data.extract_dependent()
+	print(dependent_variable)
 
 	# check the data characteristics
 
 	# create model and return the information
-
-	pass
